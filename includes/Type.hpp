f187cba37dfbbd4dd0f403e7b1dca33354f85ed8
OUAFABULOUS <ouafae1310@gmail.com>
2023-02-03 18:45:55 +0100

    <feat>: updating the type.hpp

--- includes/Type.hpp
+++ includes/Type.hpp
@@ -2,10 +2,10 @@
 #ifndef TYPE_HPP
 #define TYPE_HPP
 
-#include <string>
 #include <map>
-#include <vector>
 #include <netinet/in.h>
+#include <string>
+#include <vector>
 
 // IO
 
@@ -33,36 +33,73 @@ struct t_network_address
 
 // ROUTER && ROUTE
 
-typedef enum {
+typedef enum
+{
 	GET = 0b001,
 	POST = 0b010,
 	DELETE = 0b100
-}	t_methods;
+} t_methods;
 
-typedef struct s_attributes {
-	t_methods							methods_allowed;	// could be GET POST DELETE
-	std::vector<std::string>			server_names;		// defined by header field "Host"
-	size_t								max_body_length;
-	std::map<uint, std::string>			error_files;		// path to default error pages
-	std::string							redirect;			// uri for redirection,
-	std::string							root;				// path to root directory for serving static files
-	std::map<std::string, std::string>	cgi_path;			// path to CGI locations
-	bool								directory_listing;	// autoindex on/off
-}	t_attributes;
+typedef struct s_attributes
+{
+	t_methods methods_allowed;			   // could be GET POST DELETE
+	std::vector<std::string> server_names; // defined by header field "Host"
+	std::string location;				   // path requested in the request line
+	size_t max_body_length;
+	std::map<uint, std::string> error_files;	 // path to default error pages
+	std::string redirect;						 // uri for redirection,
+	std::string root;							 // path to root directory for serving static files
+	std::map<std::string, std::string> cgi_path; // path to CGI locations
+	bool directory_listing;						 // autoindex on/off
+} t_attributes;
 
-typedef enum s_l_tok_type
+typedef enum s_l_tok
 {
 	TOK_WORD,
-	TOK_SEP,
+	TOK_SP,
 	TOK_RL,
 	TOK_SC,
 	TOK_BR,
-} t_t_tok_type;
+} t_t_tok;
 
-typedef enum s_b_tok_type
+typedef enum s_b_tok
 {
 	TOK_BLOCK,
 	TOK_DIRECTIVE,
-} t_b_tok_type;
+} t_b_tok;
+
+typedef enum e_chr_class
+{
+	CHR_SP,
+	CHR_WORD,
+	CHR_RL,
+	CHR_SC,
+	CHR_BR,
+} t_chr_class;
+
+static const t_chr_class g_get_chr_class[255] =
+	{
+		[' '] = CHR_SP,
+		['\t'] = CHR_SP,
+		[';'] = CHR_SC,
+		['}'] = CHR_BR,
+		['!' ... ':'] = CHR_WORD,
+		[';'] = CHR_SC,
+		['<' ... 'z'] = CHR_WORD,
+		['{'] = CHR_BR,
+		['|'] = CHR_WORD,
+		['}'] = CHR_BR,
+		['~'] = CHR_WORD,
+};
+
+static const t_t_tok		g_get_tok_type[5] = {
+	[CHR_SP] = TOK_SP,
+	[CHR_WORD] = TOK_WORD,
+	[CHR_RL] = TOK_RL,
+	[CHR_SC] = TOK_SC,
+	[CHR_BR] = TOK_BR,
+};
+
+
 
 #endif
