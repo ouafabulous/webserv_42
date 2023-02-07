4de5dd98e125e055dcc38c07e21fb066321f71dd
DimiOui <omygoditsdimi@gmail.com>
2023-02-07 14:51:18 +0100

    <fix> Init list fixed, CGI constructor fixed

--- srcs/Ressource/CGI.cpp
+++ srcs/Ressource/CGI.cpp
@@ -1,36 +1,45 @@
 #include <Ressource.hpp>
 
-CGI::CGI(Connexion *conn, std::string cgi_path) :	conn(conn), response(), is_EOF(false)
+CGI::CGI(Connexion *conn, std::string cgi_path) :	conn(conn), is_EOF(false)
 {
-	int		pipefd[2];
-	
-	if (pipe(pipefd) == -1)
-		throw std::runtime_error("pipe");
+
+
+	int		pipe_in[2];
+	int		pipe_out[2];
+
+	if (pipe(pipe_in) == -1 || pipe(pipe_out) == -1)
+		throw std::runtime_error("Pipe failed.");
 
 	pid_t	pid = fork();
 
 	if (pid < 0)
 	{
-		close(pipefd[0]);
-		close(pipefd[1]);
-		throw std::runtime_error("fork");
+		close(pipe_in[0]);
+		close(pipe_in[1]);
+		close(pipe_out[0]);
+		close(pipe_out[1]);
+		throw std::runtime_error("Fork failed");
 	}
 	else if (pid == 0)
 	{
-		close(pipefd[0]);
-		dup2(pipefd[1], STDOUT_FILENO);
-		close(pipefd[1]);
+		close(pipe_out[1]);
+		close(pipe_in[0]);
+		dup2(pipe_out[0], STDIN_FILENO);	// child closes write, redirects stdin to read
+		dup2(pipe_in[1], STDOUT_FILENO);	// child closes read, redirects stdout to write
+		close(pipe_out[0]);
+		close(pipe_in[1]);
 
 		setenv("REQUEST_METHOD", conn->t_http_message.t_request_line.method.c_str(), 1);
-		setenv("QUERY_STRING", conn->t_http_message.body.c_str(), 1);
+		setenv("QUERY_STRING", conn->t_http_message.body.c_str(), 1); // no idea if c_str() will work with a vector
 
 		execve(cgi_path.c_str(), NULL, NULL);
 	}
 	else
 	{
-		close(pipefd[1]);
-		fd_read = pipefd[0];
-		fd_write = pipefd[1];
+		close(pipe_out[0]);
+		close(pipe_in[1]);
+		fd_read = pipe_in[0];
+		fd_write = pipe_out[1];
 	}
 }
 
