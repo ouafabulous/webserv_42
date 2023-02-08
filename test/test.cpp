dda8ae685e002a8d5652b292d6d55ddc562efd0b
Maxime Riaud <maxime.riaud@gmail.com>
2023-02-08 10:17:04 +0100

    <fix>[CGI_TEST]: wip

--- test/test.cpp
+++ test/test.cpp
@@ -4,18 +4,26 @@
 #include <cstdio>
 #include <vector>
 #include <cstdlib>
+#include <sys/types.h>
+#include <sys/wait.h>
 
-std::string	cgi_path = "./php-cgi/script.py";
-int			fd_read, fd_write;
-char		*args[] = { const_cast<char*>(cgi_path.c_str()), NULL };
+#define READ 0
+#define WRITE 1
 
-void	pipe_communication(int &fd_read, int &fd_write, pid_t &pid)
+std::string cgi_path = "/usr/bin/python3";
+std::string script_path = "./php-cgi/script.py";
+char *args[] = {const_cast<char *>(cgi_path.c_str()), const_cast<char *>(script_path.c_str())};
+
+void pipe_communication(char **env)
 {
-	std::string	buffer;
-	int	pipe_in[2];
-	int	pipe_out[2];
+	pid_t pid;
+	int fd_read, fd_write;
+	std::string buffer;
+	int pipe_host_to_cgi[2];
+	int pipe_cgi_to_host[2];
+	int wstatus;
 
-	if (pipe(pipe_in) == -1 || pipe(pipe_out) == -1)
+	if (pipe(pipe_host_to_cgi) == -1 || pipe(pipe_cgi_to_host) == -1)
 	{
 		perror("pipe");
 		exit(1);
@@ -23,24 +31,24 @@ void	pipe_communication(int &fd_read, int &fd_write, pid_t &pid)
 	pid = fork();
 	if (pid < 0)
 	{
-		close(pipe_in[0]);
-		close(pipe_in[1]);
-		close(pipe_out[0]);
-		close(pipe_out[1]);
+		close(pipe_host_to_cgi[READ]);
+		close(pipe_host_to_cgi[WRITE]);
+		close(pipe_cgi_to_host[READ]);
+		close(pipe_cgi_to_host[WRITE]);
 		perror("fork");
 		exit(1);
 	}
 	else if (pid == 0)
 	{
-		close(pipe_out[1]);
-		close(pipe_in[0]);
-		dup2(pipe_out[0], STDIN_FILENO);
-		dup2(pipe_in[1], STDOUT_FILENO);
-		close(pipe_out[0]);
-		close(pipe_in[1]);
+		close(pipe_host_to_cgi[READ]);
+		close(pipe_cgi_to_host[WRITE]);
+		dup2(pipe_host_to_cgi[WRITE], STDOUT_FILENO);
+		dup2(pipe_cgi_to_host[READ], STDIN_FILENO);
+		close(pipe_host_to_cgi[WRITE]);
+		close(pipe_cgi_to_host[READ]);
 
 		if (!cgi_path.empty())
-			execve(cgi_path.c_str(), args, NULL);
+			execve(cgi_path.c_str(), args, env);
 		else
 		{
 			std::cerr << "Error: CGI path is empty." << std::endl;
@@ -51,18 +59,30 @@ void	pipe_communication(int &fd_read, int &fd_write, pid_t &pid)
 	{
 		char buffer[1024];
 
-		close(pipe_out[0]);
-		close(pipe_in[1]);
-		fd_read = pipe_in[0];
-		fd_write = pipe_out[1];
-		while (read(fd_write, buffer, 1024) != 0)
+		close(pipe_host_to_cgi[WRITE]);
+		close(pipe_cgi_to_host[READ]);
+		fd_write = pipe_host_to_cgi[READ];
+		fd_read = pipe_cgi_to_host[WRITE];
+		// std::string request = "POST /path HTTP/1.1\r\n"
+		// 					  "Host: example.com\r\n"
+		// 					  "Content-Type: application/x-www-form-urlencoded\r\n"
+		// 					  "Content-Length: 25\r\n"
+		// 					  "\r\n"
+		std::string request_body = "first_name=Maxime&last_name=Riaud\r\n";
+		write(fd_write, request_body.c_str(), request_body.length());
+		while (read(fd_read, buffer, 1024) > 0)
 			std::cout << buffer << std::endl;
+
+		wait(&wstatus);
+		close(pipe_host_to_cgi[READ]);
+		close(pipe_cgi_to_host[WRITE]);
 	}
 }
 
-int main()
+int main(int ac, char **av, char **env)
 {
-	pid_t	pid;
-	pipe_communication(fd_read, fd_write, pid);
+	(void)ac;
+	(void)av;
+	pipe_communication(env);
 	return 0;
 }
