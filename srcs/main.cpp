35d25eca0cab4cb1f91b4e1ef037ea4ef1acc79e
OUAFABULOUS <ouafae1310@gmail.com>
2023-03-03 14:09:44 +0100

    <feat>: Added extension check + permissions for the config file in main

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -10,10 +10,22 @@
 #include "signal.h"
 #include "stdlib.h"
 
-void handle_sigint(int signal) {
+void handle_sigint(int signal)
+{
 	(void)signal;
 	std::cout << "\n";
 	throw std::runtime_error("Shutdown server\n");
+}
+
+void hasConfExtension(std::string const &filename)
+{
+	std::string extension = ".conf";
+	if (filename.length() >= extension.length()){
+		if (filename.compare(filename.length() - extension.length(), extension.length(), extension))
+			throw std::runtime_error("Configuration file must have .conf extension\n");
+	}
+	else
+		throw std::runtime_error("Configuration file must have .conf extension\n");
 }
 
 int main(int ac, char *av[])
@@ -22,12 +34,15 @@ int main(int ac, char *av[])
 
 	// signal(SIGPIPE, handle_sigpipe);
 	signal(SIGINT, handle_sigint);
-	if (ac != 2) {
-		Logger::error << "configuration file path required" << std::endl;
-		return (1);
-	}
-
-	try {
+	try
+	{
+		//basic checkings of the conf file
+		if (ac != 2) 									//1- if more we haven't received one argument !
+			throw std::runtime_error("Usage: ./webserv config_file\n");
+		hasConfExtension(av[1]);						//2- if the config file has .conf extension !
+		if (!checkPermissions(av[1], R_OK))				//3- if the conf file has read permissions !
+			throw std::runtime_error("Read permission is required for the config file!\n");
+		
 		// read file
 		std::ifstream file(av[1]);
 		std::stringstream buffer;
@@ -35,13 +50,19 @@ int main(int ac, char *av[])
 		std::string big_buffer = buffer.str();
 
 		// lexing
-		Lexer	lex(big_buffer);
+		Lexer lex(big_buffer);
 		lex.fillTokens();
-		Parser	parse(lex.getTokens());
+
+		// parsing
+		Parser parse(lex.getTokens());
+		
+		//server declaration
 		Server my_server(parse);
+		
+		//routine
 		my_server.routine();
 	}
-	catch(const std::exception& e) {
+	catch (const std::exception &e){
 		Logger::error << e.what();
 	}
 }
