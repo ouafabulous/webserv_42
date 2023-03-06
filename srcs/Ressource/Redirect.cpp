b04534aa8d49dab6eec948cae7eb8549ce9bae98
OUAFABULOUS <ouafae1310@gmail.com>
2023-03-06 18:05:01 +0100

    <feat>: redirect working

--- srcs/Ressource/Redirect.cpp
+++ srcs/Ressource/Redirect.cpp
@@ -2,24 +2,17 @@
 
 RedirectRessource::RedirectRessource(Connexion *conn, std::string const &url) : Ressource(conn)
 {
-	// append header
 	std::string body = "<html>\n";
-	body += "HTTP/1.1 301 Moved Permanently\r\n";
-	body += "Location: " + url + "\r\n";
-	body += "Content-Type: text/html\r\n";
-	body += "\r\n";
+    body += "<head>\n";
+    body += "<title>301 Moved Permanently</title>\n";
+    body += "</head>\n";
+    body += "<body>\n";
+    body += "<h1>Moved Permanently</h1>\n";
+    body += "<p>The document has moved <a href=\"" + url + "\">here</a>.</p>\n";
+    body += "</body>\n";
+    body += "</html>\n";
 
-	// append body
-	body += "<html>\n";
-	body += "<head>\n";
-	body += "<title>301 Moved Permanently</title>\n";
-	body += "</head>\n";
-	body += "<body>\n";
-	body += "<h1>Moved Permanently</h1>\n";
-	body += "<p>The document has moved <a href=\"" + url + "\">here</a>.</p>\n";
-	body += "</body>\n";
-	body += "</html>\n";
-
+	conn->pushResponse(http_header_formatter(301, body.length(), "text/html"));
 	conn->pushResponse(body);
 	conn->setRespEnd();
 }
